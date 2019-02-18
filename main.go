20d9899e6190b84ca52070c08376a89a55e4f0b8
Vasyl Kazimirov <kazimirov.v@gmail.com>
2019-02-19 01:11:23 +0200

    Implement main unoptimized functionality as console tool. Add local test methods. Write main todos.

---
+++ main.go
@@ -0,0 +1,189 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"compress/gzip"
+	"context"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"regexp"
+	"strings"
+	"time"
+
+	"cloud.google.com/go/storage"
+	"google.golang.org/api/option"
+	//gzip "github.com/klauspost/pgzip"
+)
+
+const bucketName = "kubernetes-jenkins"
+
+// TODO #1 Setup pgzip
+// TODO #2 Make line processing parallel, optimize it
+// TODO #3 Split code in different files
+// TODO #4 Convert manual test methods below into automated tests
+
+func main() {
+	logPath, targetSubstring := setupFromConsole()
+	regex, _ := regexp.Compile(targetSubstring)
+
+	bytess := downloadAndDecompress(logPath)
+	reader := bufio.NewReader(bytes.NewReader(bytess))
+	parsed, _ := processLines(reader, regex)
+
+	if len(parsed) == 0 {
+		fmt.Printf("No lines found")
+	} else {
+		for _, line := range parsed {
+			fmt.Println(*line)
+		}
+	}
+}
+
+const testLogPath = "logs/ci-kubernetes-e2e-gce-scale-performance/290/artifacts/gce-scale-cluster-master/kube-apiserver-audit.log-20190102-1546444805.gz"
+const testTargetSubstring = "FrequentKubeletRestart"
+
+func setupFromConsole() (string, string) {
+	var logPath, targetSubstring string
+	if len(os.Args) < 2 || os.Args[1] == "" {
+		logPath = testLogPath
+	} else {
+		logPath = os.Args[1]
+	}
+
+	if len(os.Args) < 3 || os.Args[2] == "" {
+		targetSubstring = testTargetSubstring
+	} else {
+		targetSubstring = os.Args[2]
+	}
+
+	return logPath, targetSubstring
+}
+
+func processLines(rd *bufio.Reader, regex *regexp.Regexp) ([]*logEntry, error) {
+	var result []*logEntry
+	defer timeTrack(time.Now(), "filtering and parsing")
+	for {
+		line, err := rd.ReadBytes('\n')
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return nil, err
+		}
+		isMatched, entry, err := processLine(line, regex)
+		if isMatched && err == nil {
+			result = append(result, entry)
+		}
+	}
+	return result, nil
+}
+
+func processLine(line []byte, regex *regexp.Regexp) (bool, *logEntry, error) {
+	if !regex.Match(line) {
+		return false, nil, nil
+	}
+
+	parsed, err := parseLine(string(line))
+	return true, parsed, err
+}
+
+type logEntry struct {
+	log  string
+	time string
+}
+
+// TODO Parse as bytes if needed
+func parseLine(line string) (*logEntry, error) {
+	const startMarker = "ReceivedTimestamp\":\""
+	const endMarker = "\",\"stageTimestamp"
+	start := strings.Index(line, startMarker)
+	end := strings.Index(line, endMarker)
+	if start == -1 {
+		return &logEntry{}, &parseLineFailedError{line}
+	}
+	if end == -1 {
+		return &logEntry{}, &parseLineFailedError{line}
+	}
+	timestamp := line[(start + len(startMarker)):end]
+	return &logEntry{log: line, time: timestamp}, nil
+}
+
+type parseLineFailedError struct {
+	line string
+}
+
+func (e *parseLineFailedError) Error() string {
+	return "Failed to parse line: " + e.line
+}
+
+func downloadAndDecompress(objectPath string) []byte {
+	bts, err := download(objectPath)
+	handle(err)
+
+	decompressed, err := decompress(bts)
+	handle(err)
+	return decompressed
+}
+
+func decompress(bts []byte) ([]byte, error) {
+	defer timeTrack(time.Now(), "decompress")
+	reader, err := gzip.NewReader(bytes.NewReader(bts))
+	if err != nil {
+		return nil, err
+	}
+	return ioutil.ReadAll(reader)
+}
+
+func download(objectPath string) ([]byte, error) {
+	context := context.Background()
+	client, err := storage.NewClient(context, option.WithoutAuthentication())
+	handle(err)
+
+	bucket := client.Bucket(bucketName)
+
+	remoteFile := bucket.Object(objectPath).ReadCompressed(true)
+	reader, err := remoteFile.NewReader(context)
+	handle(err)
+
+	defer timeTrack(time.Now(), "download")
+	localBytes, err := ioutil.ReadAll(reader)
+	return localBytes, err
+}
+
+func timeTrack(start time.Time, name string) {
+	elapsed := time.Since(start)
+	log.Printf("%s took %s", name, elapsed)
+}
+
+func handle(err error) {
+	if err == nil {
+		return
+	}
+	log.Panic(err)
+}
+
+func readFromLocalFile(filename string) io.Reader {
+	bts, err := ioutil.ReadFile(filename)
+	handle(err)
+	return bytes.NewReader(bts)
+}
+
+const testFilePath = "c:\\temp\\kube-apiserver.log"
+
+func testDownloadToLocalFile() {
+	path := testLogPath
+	bytess := downloadAndDecompress(path)
+	err := ioutil.WriteFile(testFilePath, bytess, 0644)
+	handle(err)
+}
+
+func testParsingOnLocalFile() {
+	regex, _ := regexp.Compile(testTargetSubstring)
+	reader := bufio.NewReader(readFromLocalFile(testFilePath))
+	parsed, _ := processLines(reader, regex)
+	log.Println(parsed)
+}
