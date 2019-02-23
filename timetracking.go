63ae1da2b45dae4f5bc78c8450ff81aa17cc2fb0
Vasyl Kazimirov <kazimirov.v@gmail.com>
2019-02-23 21:46:53 +0200

    Move time tracking stuff to separate file

---
+++ timetracking.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"errors"
+	"io"
+	"log"
+	"sync"
+	"time"
+)
+
+var durations = newDurationMap()
+
+type trackingReader struct {
+	inner io.Reader
+	name  string
+}
+
+func newTrackingReader(reader io.Reader, name string) io.Reader {
+	return &trackingReader{
+		inner: reader,
+		name:  name}
+}
+
+func (reader *trackingReader) Read(bts []byte) (int, error) {
+	if reader.inner == nil {
+		return 0, errors.New("Need to initialize with reader")
+	}
+	defer timeTrackIncremental(time.Now(), reader.name)
+	n, err := reader.inner.Read(bts)
+	return n, err
+}
+
+func timeTrack(start time.Time, name string) {
+	elapsed := time.Since(start)
+	log.Printf("%s took %s", name, elapsed)
+}
+
+func timeTrackIncremental(start time.Time, name string) {
+	elapsed := time.Since(start)
+	durations.increase(name, elapsed)
+}
+
+// Use this instead of sync.map to keep code cleaner
+type durationMap struct {
+	mx sync.Mutex
+	m  map[string]time.Duration
+}
+
+func newDurationMap() *durationMap {
+	return &durationMap{m: map[string]time.Duration{}}
+}
+
+func (mp *durationMap) increase(key string, value time.Duration) {
+	mp.mx.Lock()
+	defer mp.mx.Unlock()
+	mp.m[key] += value
+}
+
+func (mp *durationMap) printAll() {
+	for name, elapsed := range mp.m {
+		log.Printf("%s took %s", name, elapsed)
+	}
+}
