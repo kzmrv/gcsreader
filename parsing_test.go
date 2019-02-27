e71b7c125bfebb9ae9d07fe28a0260eb981b7988
Vasyl Kazimirov <kazimirov.v@gmail.com>
2019-02-28 00:54:21 +0200

    Add some basic tests

---
+++ parsing_test.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bytes"
+	"io/ioutil"
+	"path/filepath"
+	"regexp"
+	"testing"
+)
+
+func Test_ShouldMatchSingleLine(t *testing.T) {
+	data, err := ioutil.ReadFile(filepath.Join("testdata", "kube-apiserver-cut.log"))
+	regex := regexp.MustCompile("0286b87c")
+	if err != nil {
+		t.Fatal(err)
+	}
+	lines, err := processLines(bytes.NewReader(data), regex)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(lines) != 1 {
+		t.Fatalf("Expected 1 line, got %v", len(lines))
+	}
+}
+
+func Test_ShouldParseLine(t *testing.T) {
+	data, err := ioutil.ReadFile(filepath.Join("testdata", "kube-apiserver-cut.log"))
+	lineStr := string(data[:983])
+	t.Log(lineStr)
+	line, err := parseLine(lineStr)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	const expectedTime = "2019-01-02T15:01:16.105964Z"
+	if line.time != expectedTime {
+		t.Fatalf("Expected time %s received %s", expectedTime, line.time)
+	}
+
+	if line.log != lineStr {
+		t.Fatalf("Expected log %s received %s", lineStr, line.log)
+	}
+}
